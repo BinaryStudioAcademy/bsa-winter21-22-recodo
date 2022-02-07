28996d93d4ba1f854ea5b158824f7643183023ad
aydevelop <juwric@gmail.com>
2022-02-07 20:37:12 +0200

    Add desktop panel

--- RecodoDesktop/RecodoDesktop/MainWindow.xaml.cs
+++ RecodoDesktop/RecodoDesktop/MainWindow.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RecodoDesktop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.Top = 0;
+            this.Left = 0;
+            this.Topmost = true;
+            this.ShowInTaskbar = true;
+            this.AllowsTransparency = true;
+            this.WindowStyle = WindowStyle.None;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.DragMove();
+
+            }
+            catch (System.Exception)
+            { }
         }
     }
 }
