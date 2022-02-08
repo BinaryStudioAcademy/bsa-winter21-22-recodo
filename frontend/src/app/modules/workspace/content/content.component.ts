305363a9fbc59130052023c83cec5c8c15eec9fe
VolodymyrIvanitskyi <vovanchuk2003@gmail.com>
2022-02-08 20:13:54 +0200

    created personal page

--- frontend/src/app/modules/workspace/content/content.component.ts
+++ frontend/src/app/modules/workspace/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-content',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) { 
+    this.matIconRegistry.addSvgIcon(
+      "details",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Details.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "share-item",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/ShareItem.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "star",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Star.svg")
+    );
+  }
 
   ngOnInit(): void {
   }
