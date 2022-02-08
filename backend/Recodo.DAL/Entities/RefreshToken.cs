14efc56e47ae722f36ed6d493200c4bb27491706
VitalikMatvieiev <77060767+VitalikMatvieiev@users.noreply.github.com>
2022-02-08 17:41:06 +0200

    Update backend/Recodo.DAL/Entities/RefreshToken.cs
    Co-authored-by: Yurii Palaida <yura.palayda@gmail.com>

--- backend/Recodo.DAL/Entities/RefreshToken.cs
+++ backend/Recodo.DAL/Entities/RefreshToken.cs
@@ -10,7 +10,7 @@ namespace Recodo.DAL.Entities
     {
         public int Id { get; set; }
         public string Token { get; set; }
-        public DateTime Expires { get; set; }
+        public DateTime ExpiresAt { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public DateTime CreatedAt { get; set; }
