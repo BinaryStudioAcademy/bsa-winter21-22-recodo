80c857f9dc0e93329412616f3932a7adb15d3814
VolodymyrIvanitskyi <vovanchuk2003@gmail.com>
2022-02-09 19:11:24 +0200

    took away the ability to open and close sidebar

--- frontend/src/app/modules/workspace/base/base.component.ts
+++ frontend/src/app/modules/workspace/base/base.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout'
 import { CustomIconService } from 'src/app/services/custom-icon.service';
 
 @Component({
@@ -9,31 +8,11 @@ import { CustomIconService } from 'src/app/services/custom-icon.service';
 })
 export class BaseComponent implements OnInit {
 
-  public sidebarOpen:boolean = true;
-  
-  constructor(
-    private observer:BreakpointObserver,
-    private customService:CustomIconService) 
-    { 
+  constructor(private customService:CustomIconService) { 
     this.customService.init();
   }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(){
-    this.observer.observe(['(max-width:800px)']).subscribe(
-      (res)=>{
-        if(res.matches){
-          this.sidebarOpen=false;
-        }
-        else{
-          this.sidebarOpen = true;
-        }
-      }
-    );
-  }
-  public sidebarToogler(){
-    this.sidebarOpen = !this.sidebarOpen;
-  }
 }
