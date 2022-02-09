14736328018c07e2236a450a8cb0bd4fb03530da
VolodymyrIvanitskyi <vovanchuk2003@gmail.com>
2022-02-09 19:05:17 +0200

    change route to workspace from 'home' to ''

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path:'home', loadChildren: () => import('./modules/workspace/workspace.module').then(m => m.WorkspaceModule)}
+  { path: '', loadChildren: () => import('./modules/workspace/workspace.module').then(m => m.WorkspaceModule) }
 ];
 
 @NgModule({
