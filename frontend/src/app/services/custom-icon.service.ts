07e3fde6e85e0bd90c565163701e49a93a13f3f8
VolodymyrIvanitskyi <vovanchuk2003@gmail.com>
2022-02-09 14:22:28 +0200

    Replaced all the icons from assets/ to assets/icons/ folder

--- frontend/src/app/services/custom-icon.service.ts
+++ frontend/src/app/services/custom-icon.service.ts
@@ -16,47 +16,47 @@ export class CustomIconService {
     //Sidebar icons
     this.matIconRegistry.addSvgIcon(
       "personal",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Personal.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Personal.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "share",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Share.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Share.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "settings",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Settings.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Settings.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "team",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Team.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Team.svg")
     );
 
     //Horizontalbar icons
     this.matIconRegistry.addSvgIcon(
       "search",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Search.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Search.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "bell",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Bell.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Bell.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "menu",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Menu.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Menu.svg")
     );
 
     //Personal page icons
     this.matIconRegistry.addSvgIcon(
       "details",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Details.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Details.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "share-item",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/ShareItem.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/ShareItem.svg")
     );
     this.matIconRegistry.addSvgIcon(
       "star",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/Star.svg")
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/Star.svg")
     );
   }
 }
