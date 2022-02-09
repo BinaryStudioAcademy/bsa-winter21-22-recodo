a677a5d6f9dcf6e9b4f418a2d0dc41b39213fc33
aydevelop <juwric@gmail.com>
2022-02-09 18:45:46 +0200

    Generic HTTP client

---
+++ frontend/src/app/core/models/response.ts
@@ -0,0 +1,5 @@
+export interface IResponse {
+  data: {};
+  exceptionMessage: string;
+  isError: boolean;
+}
---
+++ frontend/src/app/core/services/resource.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { IResponse } from '../models/response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export abstract class ResourceService<T> {
+  private readonly APIUrl = environment.apiUrl + this.getResourceUrl();
+
+  constructor(protected httpClient: HttpClient) {}
+
+  abstract getResourceUrl(): string;
+
+  toServerModel(entity: T): string {
+    return JSON.stringify(entity);
+  }
+
+  fromServerModel(json: IResponse): T {
+    return json.data as T;
+  }
+
+  getList(index: number, page: number): Observable<T[]> {
+    let params = new HttpParams()
+      .set('limit', index.toString())
+      .set('offset', page.toString());
+
+    return this.httpClient
+      .get<IResponse[]>(`/${this.APIUrl}?${params.toString()}`)
+      .pipe(
+        map((list) => list.map((item) => this.fromServerModel(item))),
+        catchError(this.handleError)
+      );
+  }
+
+  get(id: string | number): Observable<T> {
+    return this.httpClient.get<IResponse>(`/${this.APIUrl}/${id}`).pipe(
+      map((item) => this.fromServerModel(item)),
+      catchError(this.handleError)
+    );
+  }
+
+  add(resource: T): Observable<IResponse> {
+    return this.httpClient
+      .post<IResponse>(`/${this.APIUrl}`, this.toServerModel(resource))
+      .pipe(catchError(this.handleError));
+  }
+
+  delete(id: string | number): Observable<IResponse> {
+    return this.httpClient
+      .delete<IResponse>(`/${this.APIUrl}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  update(resource: T) {
+    return this.httpClient
+      .put(`/${this.APIUrl}`, this.toServerModel(resource))
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    return throwError(() => error);
+  }
+}
