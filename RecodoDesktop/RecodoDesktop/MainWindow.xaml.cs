b0cc5dc7c00b9bbb5cd87154a9fc188ded7d26e1
aydevelop <juwric@gmail.com>
2022-02-10 17:36:18 +0200

    Move the stick panel to a separate file

--- RecodoDesktop/RecodoDesktop/MainWindow.xaml.cs
+++ RecodoDesktop/RecodoDesktop/MainWindow.xaml.cs
@@ -1,36 +1,19 @@
-﻿using System.Windows;
-using System.Windows.Input;
+﻿using Recodo.Desktop.Main;
+using System.Windows;
 
 namespace RecodoDesktop
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Interaction logic for MainWindow.xaml
-        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            this.Top = 70;
-            this.Left = 0;
-            this.Topmost = true;
-            this.ShowInTaskbar = true;
-        }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            try
-            {
-                this.DragMove();
-
-            }
-            catch (System.Exception)
-            { }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+            StickPanel stickPanel = new();
+            stickPanel.Show();
         }
     }
 }
