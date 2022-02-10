2f32202332dd8e821c431f0694cfd51d76dbf0d4
VitalikMatvieiev <77060767+VitalikMatvieiev@users.noreply.github.com>
2022-02-10 11:24:22 +0200

    Add fluent API and Initial migration (#13)
    * Add fluent API and Initial migration
    
    * fix
    
    * Update backend/Recodo.API/Startup.cs
    
    Co-authored-by: Yurii Palaida <yura.palayda@gmail.com>
    
    * feedback1
    
    * Change migration
    
    * add ownes entites
    
    Co-authored-by: Yurii Palaida <yura.palayda@gmail.com>

---
+++ backend/Recodo.DAL/Entities/Configuration/NotificationConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recodo.DAL.Entities.Configuration
+{
+    public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Message).IsRequired();
+            builder.Property(p => p.ReceiverId).IsRequired();
+            builder.HasOne<User>().WithMany().HasForeignKey(p => p.ReceiverId);
+            builder.Property(p => p.CreatedAt).IsRequired();
+            builder.Property(p => p.Type).IsRequired();
+            builder.Property(p => p.IsRead).IsRequired();
+        }
+    }
+}
