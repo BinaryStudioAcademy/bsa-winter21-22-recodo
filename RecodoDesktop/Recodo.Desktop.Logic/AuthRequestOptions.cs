55d886a516d3a4068c70bb834c177e9cb0b578a9
vasylmrnchk <mrvasylmyronchuk@gmail.com>
2022-02-13 18:45:44 +0200

    Add Base64Url string generator

--- RecodoDesktop/Recodo.Desktop.Logic/AuthRequestOptions.cs
+++ RecodoDesktop/Recodo.Desktop.Logic/AuthRequestOptions.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Recodo.Desktop.Logic
 {
     public class AuthRequestOptions
-    {        
+    {
+        const uint bufferSize = 64;
         const string clientId = "sample-client-id";
         const string codeChallengeMethod = "S256";
         const string authorizationEndpoint = "https://oauth.mocklab.io/oauth/authorize";
         const string tokenEndpoint = "https://oauth.mocklab.io/oauth/token";
         const string userInfoEndpoint = "https://oauth.mocklab.io/userinfo";
 
-        string state;
-        string codeVerifier;
-        string codeChallenge;
+        readonly string state;
+        readonly string codeVerifier;
+        readonly string codeChallenge;
 
         public string RedirectUrl { get; set; }
         public AuthRequestOptions()
         {
-            
+            state = Base64UrlString.RandomBase64UrlString(bufferSize);
+            codeVerifier = Base64UrlString.RandomBase64UrlString(bufferSize);
+            codeChallenge = Base64UrlString.Base64UrlEncodeNoPadding(Base64UrlString.Sha256(codeVerifier));
         }
 
         public string GetAuthRequestUrl()
---
+++ RecodoDesktop/Recodo.Desktop.Logic/Base64UrlString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Recodo.Desktop.Logic
+{
+    public class Base64UrlString
+    {
+        public static string RandomBase64UrlString(uint length)
+        {
+            var rng = RandomNumberGenerator.Create();
+            var bytes = new byte[length];
+            rng.GetBytes(bytes);
+            return Base64UrlEncodeNoPadding(bytes);
+        }
+
+        public static byte[] Sha256(string inputString)
+        {
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string Base64UrlEncodeNoPadding(byte[] buffer)
+        {
+            return Convert.ToBase64String(buffer)
+                    .TrimEnd('=')
+                    .Replace('+', '-')
+                    .Replace('/', '_');
+        }
+    }
+}
