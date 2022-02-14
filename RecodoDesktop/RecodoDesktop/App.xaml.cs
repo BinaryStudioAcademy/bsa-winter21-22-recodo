72eac1b20d65325ea4e75a84c4ae0db6c0d2b6e6
Vasyl Myronchuk <mrvasylmyronchuk@gmail.com>
2022-02-14 10:08:42 +0200

    Add desktop form for recording videos (#7)
    * Created form layout and added icons
    
    * Customized basic theme colors brush
    
    * Finished programm logo and added wrappers to header panel
    
    * Changed FontFamily to Material font family
    
    * Added wrappers to Recording Settings items
    
    * Changed Material Font to Poppins
    
    * Disabled standart border style and add drag on left mouse down
    
    * Added internet status badge
    
    * Fixed accent buttons color hue
    
    * Update RecodoDesktop/RecodoDesktop/VideoRecordingForm.xaml
    
    Co-authored-by: Yurii Palaida <yura.palayda@gmail.com>
    
    * Fixed formatting
    
    * Combobox common style
    
    * Fix Screenshot button content padding
    
    * Show the form with MainWindow on startup
    
    * Fixed screenCamera combobbox margin
    
    * Fix  trasparent  combobox items panel
    
    * Add corner radius to buttons
    
    * Rounded window corners
    
    * Rounded combobox corners
    
    * Changed position of combobox select arrow
    
    * Change combobox style to outlined and fix popup panel width
    
    * Fix toolbar buttons margin
    
    * Change internet speed combobox width
    
    Co-authored-by: Yurii Palaida <yura.palayda@gmail.com>

--- RecodoDesktop/RecodoDesktop/App.xaml.cs
+++ RecodoDesktop/RecodoDesktop/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recodo.Desktop.Main;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,10 @@ namespace RecodoDesktop
     /// </summary>
     public partial class App : Application
     {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            VideoRecordingForm videoRecordingForm = new ();
+            videoRecordingForm.Show();
+        }
     }
 }
