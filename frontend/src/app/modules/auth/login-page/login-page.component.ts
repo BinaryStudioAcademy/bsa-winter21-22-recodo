a8adc22d49f741d1f00f98c52cf0ba0cf7da5cd0
VolodymyrIvanitskyi <vovanchuk2003@gmail.com>
2022-02-15 12:46:20 +0200

    imported OnInit

--- frontend/src/app/modules/auth/login-page/login-page.component.ts
+++ frontend/src/app/modules/auth/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { UserLoginDto } from 'src/app/models/auth/user-login-dto';
 import { LoginService } from 'src/app/services/login.service';
@@ -9,21 +9,21 @@ import { Router } from '@angular/router';
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent{
+export class LoginPageComponent implements OnInit {
 
   @Input() userLoginDto : UserLoginDto = {} as UserLoginDto;
   public loginForm : FormGroup = {} as FormGroup;
 
   public hidePass = true;
-  public hideConfirmPass = true;  
+  public hideConfirmPass = true;
 
   constructor(
     private router : Router,
     private formBuilder : FormBuilder,
     private loginService : LoginService
-  ) { }  
+  ) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.loginForm = this.formBuilder.group({
       email: [, {
         validators: [
@@ -40,12 +40,11 @@ export class LoginPageComponent{
         ],
         updateOn: 'change'
       }],
-    }); 
+    });
   }
 
   public signIn(_user : UserLoginDto) {
-    this.loginService.login(_user).subscribe((resp) => 
-    {
+    this.loginService.login(_user).subscribe(() => {
       if(this.loginService.areTokensExist()) {
         this.router.navigate(['me/']);
         console.log(localStorage.getItem('accessToken'));
