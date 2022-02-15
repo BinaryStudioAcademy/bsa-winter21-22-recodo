74de48a453cb2d568de26e262d3e9a93f98e081e
shmato4ek <58270694+shmato4ek@users.noreply.github.com>
2022-02-15 02:31:15 +0200

    Add log out context menu (#44)

--- frontend/src/app/modules/workspace/navbar/navbar.component.ts
+++ frontend/src/app/modules/workspace/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @ViewChild(MatMenuTrigger)
+  contextMenu?: MatMenuTrigger;
 
-  constructor() { }
+  contextMenuPosition = { x: '0px', y: '0px' };
+  constructor() {}
+  onContextMenu(event: MouseEvent) {
+    event.preventDefault();
+    this.contextMenuPosition.x = window.outerWidth - 50 + 'px';
+    this.contextMenuPosition.y = 100 + 'px';
+    this.contextMenu?.menu.focusFirstItem('mouse');
+    this.contextMenu?.openMenu();
+  }
 
+  onLogOut() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+  }
 }
