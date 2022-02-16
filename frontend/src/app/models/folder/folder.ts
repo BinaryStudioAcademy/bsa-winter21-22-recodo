68a0fa5963d56d81823e46202819fe6b8d2d0e89
vasylmrnchk <mrvasylmyronchuk@gmail.com>
2022-02-16 18:37:35 +0200

    Add frontend new folder model

--- frontend/src/app/models/folder/folder.ts
+++ frontend/src/app/models/folder/folder.ts
@@ -1,6 +1,12 @@
+import { UserDto } from '../user/user-dto';
+
 export interface Folder{
   id:number;
   name:string;
   authorId:number;
+  author:UserDto;
+  parent:Folder;
+  parentId:number;
   teamId:number;
+  subFolders:Folder[];
 }
---
+++ frontend/src/app/models/folder/new-folder.ts
@@ -0,0 +1,7 @@
+export interface NewFolder{
+  id:number;
+  name:string;
+  authorId:number;
+  parentId:number;
+  teamId:number;
+}
