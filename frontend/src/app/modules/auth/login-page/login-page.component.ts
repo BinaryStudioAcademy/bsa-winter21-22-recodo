82fd34e1f7fecc2b016e412591dd571e8bf30575
vasylmrnchk <mrvasylmyronchuk@gmail.com>
2022-02-17 21:07:50 +0200

    Add redirect to route from native app

--- frontend/src/app/modules/auth/login-page/login-page.component.ts
+++ frontend/src/app/modules/auth/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { UserLoginDto } from 'src/app/models/auth/user-login-dto';
 import { LoginService } from 'src/app/services/login.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserDto } from 'src/app/models/user/user-dto';
 
 @Component({
@@ -18,15 +18,20 @@ export class LoginPageComponent implements OnInit {
   public hidePass = true;
   public hideConfirmPass = true;
   public currentUser:UserDto = {} as UserDto;
+  redirectUrl : string |undefined;
 
   constructor(
     private router : Router,
+    private route : ActivatedRoute,
     private formBuilder : FormBuilder,
     private loginService : LoginService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.validateForm();
+    this.route.queryParams.subscribe(params => {
+      this.redirectUrl = params['redirect_url'];
+    });
   }
 
   private validateForm() {
@@ -53,7 +58,13 @@ export class LoginPageComponent implements OnInit {
     this.loginService.login(_user).subscribe((responce) => {
       this.currentUser = responce;
       if(this.loginService.areTokensExist()) {
-        this.router.navigate(['/personal']);
+        if (this.redirectUrl)
+        {
+          window.location.href= `${this.redirectUrl}?access_token=${localStorage.getItem('accessToken')}`;
+        }
+        else {
+          this.router.navigate(['/personal']);
+        }
       }
     });
   }
