18f9d79b2ad5ef9ba795f73ae9da34cc71719280
vasylmrnchk <mrvasylmyronchuk@gmail.com>
2022-02-18 11:03:48 +0200

    Added folder frontend service

---
+++ frontend/src/app/services/folder.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ResourceService } from './resource.service';
+import { FolderDto } from '../models/folder/folder-dto';
+import { NewFolderDto } from '../models/folder/new-folder-dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FolderService extends ResourceService<NewFolderDto> {
+  private folder :FolderDto = {} as FolderDto;
+
+  constructor(override httpClient: HttpClient) {
+    super(httpClient);
+  }
+
+  getResourceUrl(): string {
+    return '/folder'
+  };
+
+  public create(folder : NewFolderDto) {
+    return this.handleAuthResponse(this.add<NewFolderDto,FolderDto>(folder));
+  }
+
+  private handleAuthResponse(observable: Observable<HttpResponse<FolderDto>>) {
+    return observable.pipe(
+        map((resp) => {
+            this.folder = resp.body as FolderDto;
+            return this.folder;
+        })
+    );
+  }
+}
