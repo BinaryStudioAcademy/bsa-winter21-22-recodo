78b45d90f98ad6b7b114e5e9140821cc817d3c9b
vasylmrnchk <mrvasylmyronchuk@gmail.com>
2022-03-01 15:44:44 +0200

    Fix token return on redirect

--- frontend/src/app/modules/auth/login-page/login-page.component.ts
+++ frontend/src/app/modules/auth/login-page/login-page.component.ts
@@ -10,76 +10,82 @@ import { SnackBarService } from 'src/app/services/snack-bar.service';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-
-  @Input() userLoginDto : UserLoginDto = {} as UserLoginDto;
-  public loginForm : FormGroup = {} as FormGroup;
+  @Input() userLoginDto: UserLoginDto = {} as UserLoginDto;
+  public loginForm: FormGroup = {} as FormGroup;
 
   public hidePass = true;
   public hideConfirmPass = true;
-  public currentUser:UserDto = {} as UserDto;
-  redirectUrl : string | undefined;
+  public currentUser: UserDto = {} as UserDto;
+  redirectUrl: string | undefined;
 
   constructor(
-    private router : Router,
-    private route : ActivatedRoute,
-    private formBuilder : FormBuilder,
-    private loginService : LoginService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
     private externalAuthService: ExternalAuthService,
     private snackbarService: SnackBarService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.validateForm();
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.redirectUrl = params['redirect_url'];
     });
   }
 
   private validateForm() {
     this.loginForm = this.formBuilder.group({
-      email: [, [
+      email: [
+        ,
+        [
           Validators.required,
-          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
-        ]
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
       ],
-      password: [, [
+      password: [
+        ,
+        [
           Validators.required,
           Validators.minLength(8),
           Validators.maxLength(20),
-          Validators.pattern(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9*.!@#$%^&`(){}[\]:;<>,‘.?/~_+=|-]+)$/)
+          Validators.pattern(
+            /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9*.!@#$%^&`(){}[\]:;<>,‘.?/~_+=|-]+)$/
+          ),
         ],
       ],
     });
   }
 
   public signIn(_user: UserLoginDto) {
-    this.loginService.login(_user)
-      .subscribe((response) => {
+    this.loginService.login(_user).subscribe({
+      next: (response) => {
         this.currentUser = response;
-        if(this.loginService.areTokensExist()) {
-          if (this.redirectUrl)
-          {
-            this.router.navigate(['/']).then( () => {
-            window.location.href= `${this.redirectUrl}?access_token=${localStorage.getItem('accessToken')}`
-            });
-          }
-          else {
-            this.router.navigate(['/personal']);
-          }
+        if (this.loginService.areTokensExist()) {
+          this.router.navigate(['/personal']).then(() => {
+            if (this.redirectUrl) {
+              window.location.href = `${
+                this.redirectUrl
+              }?access_token=${localStorage.getItem('accessToken')}`;
+            }
+          });
         }
       },
-      (error) => {
+      error: (error) => {
         switch (error.status) {
           case 401:
             this.snackbarService.openSnackBar('Incorrect password');
             break;
           case 404:
-            this.snackbarService.openSnackBar('No user was found with this email');
-          }
-      });
+            this.snackbarService.openSnackBar(
+              'No user was found with this email'
+            );
+        }
+      },
+    });
   }
 
   public googleLogin = () => {
