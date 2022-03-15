282d7b8a6c3585a899aa577663c6313928ab6ce6
VolodymyrIvanitskyi <vovanchuk2003@gmail.com>
2022-03-15 17:08:43 +0200

    avatar link is taken from the DB

--- backend/Recodo.Common/Dtos/User/UserDTO.cs
+++ backend/Recodo.Common/Dtos/User/UserDTO.cs
@@ -6,5 +6,6 @@ namespace Recodo.Common.Dtos.User
         public int Id { get; set; }
         public string Email { get; set; }
         public string WorkspaceName { get; set; }
+        public string AvatarLink { get; set; }
     }
 }
