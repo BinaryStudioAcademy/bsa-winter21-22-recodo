18bcbacfc72332bb92d57bdff9f4817dae4ea0ee
aydevelop <juwric@gmail.com>
2022-03-24 16:43:55 +0200

    Fix global

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl: 'https://localhost:5001/api',
-  production: false,
+  production: true,
   googleKey:
     '597145097157-nj02iuobak24qq0pc4olek3rqbk32p20.apps.googleusercontent.com',
   clientUrl: 'https://recodo.westeurope.cloudapp.azure.com/',
