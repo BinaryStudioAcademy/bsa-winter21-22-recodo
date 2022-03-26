28d90a449d5c45b0c2d265c5bfbf127299d4d189
aydevelop <juwric@gmail.com>
2022-03-26 20:10:08 +0200

    Fixed messages reset pass and placeholder text

--- backend/Recodo.BLL/Services/EmailService.cs
+++ backend/Recodo.BLL/Services/EmailService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System;
 using System.Threading.Tasks;
 
 namespace Thread_.NET.BLL.Services
@@ -9,15 +10,23 @@ namespace Thread_.NET.BLL.Services
     {
         public static async Task SendEmailAsync(string toEmail, string subject, string message, IConfiguration configuration)
         {
-            string apiKey = configuration["SendGridKey"];
-            string fromEmail = configuration["SendGridFromEmail"];
-            var client = new SendGridClient(apiKey);
+            try
+            {
+                string apiKey = configuration["SendGridKey"];
+                string fromEmail = configuration["SendGridFromEmail"];
+                var client = new SendGridClient(apiKey);
 
-            var from = new EmailAddress(fromEmail, "Admin Recodo");
-            var to = new EmailAddress(toEmail, toEmail);
+                var from = new EmailAddress(fromEmail, "Admin Recodo");
+                var to = new EmailAddress(toEmail, toEmail);
 
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
-            await client.SendEmailAsync(msg);
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
+                await client.SendEmailAsync(msg);
+            }
+            catch (System.Exception)
+            {
+                //For debugging
+                Console.WriteLine(message);
+            }
         }
     }
 }
