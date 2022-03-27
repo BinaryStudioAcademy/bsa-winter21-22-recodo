101e11c71673ffac2db4ce15c5b91d6f30cfbf47
aydevelop <juwric@gmail.com>
2022-03-27 23:57:29 +0300

    Restore standart config settings

--- backend/Recodo.BLL/Services/EmailService.cs
+++ backend/Recodo.BLL/Services/EmailService.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System;
 using System.Threading.Tasks;
 
 namespace Thread_.NET.BLL.Services
@@ -10,23 +9,15 @@ namespace Thread_.NET.BLL.Services
     {
         public static async Task SendEmailAsync(string toEmail, string subject, string message, IConfiguration configuration)
         {
-            try
-            {
-                string apiKey = configuration["SendGridKey"];
-                string fromEmail = configuration["SendGridFromEmail"];
-                var client = new SendGridClient(apiKey);
+            string apiKey = configuration["SendGridKey"];
+            string fromEmail = configuration["SendGridFromEmail"];
+            var client = new SendGridClient(apiKey);
 
-                var from = new EmailAddress(fromEmail, "Admin Recodo");
-                var to = new EmailAddress(toEmail, toEmail);
+            var from = new EmailAddress(fromEmail, "Admin Recodo");
+            var to = new EmailAddress(toEmail, toEmail);
 
-                var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
-                await client.SendEmailAsync(msg);
-            }
-            catch (System.Exception)
-            {
-                //For debugging
-                Console.WriteLine(message);
-            }
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
+            await client.SendEmailAsync(msg);
         }
     }
 }
