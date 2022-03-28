e244c4288d1c1bff0a6f96d17aab0754c12eb53b
jyuart <yura.palayda@gmail.com>
2022-03-28 13:48:49 +0300

    working reactions

--- frontend/src/app/modules/workspace/video/video-player/video-player.component.ts
+++ frontend/src/app/modules/workspace/video/video-player/video-player.component.ts
@@ -26,10 +26,10 @@ export class VideoPlayerComponent {
     private videoUrlService: VideoUrlService,
     protected httpClient: HttpClient
   ) {
-    route.params.pipe(map((p) => p['id'])).subscribe((id) => {
-      this.videoId = id;
-      this.getVideoUrl();
-    });
+    // route.params.pipe(map((p) => p['id'])).subscribe((id) => {
+    //   this.videoId = id;
+    //   this.getVideoUrl();
+    // });
   }
 
   public getVideoUrl() {
