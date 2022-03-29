61453d1c996f8704db928d07c5878bdf1e7e65b9
Mykola Rostotskyi <1nick.rost@gmail.com>
2022-03-29 20:53:35 +0300

    add validation message and changed some text on personal page

--- frontend/src/app/modules/workspace/dialog/dialog.component.ts
+++ frontend/src/app/modules/workspace/dialog/dialog.component.ts
@@ -25,6 +25,7 @@ export class DialogComponent implements OnInit {
       folderName: [, {
         validators: [
           Validators.required,
+          Validators.minLength(3),
           Validators.maxLength(30),
         ],
         updateOn: 'change',
