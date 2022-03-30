476a6c28c9d6a3c33d07a9470246a26c547c2cd8
shmato4ek <58270694+shmato4ek@users.noreply.github.com>
2022-03-30 14:39:00 +0300

    Fixed user controller

--- backend/Recodo.API/Controllers/UserController.cs
+++ backend/Recodo.API/Controllers/UserController.cs
@@ -27,6 +27,24 @@ namespace Recodo.API.Controllers
             return Ok(user);
         }
 
+        [HttpPost]
+        [Route("Add-To-Team/{token}")]
+        public async Task<IActionResult> AddToTeam(string token)
+        {
+            await _userService.AddToTeam(this.GetUserIdFromToken(), token);
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("Send-Invite-Link/{email}")]
+        public async Task<IActionResult> SendInviteLink(string email)
+        {
+            await _teamService.SendInviteLink(this.GetUserIdFromToken(), email);
+
+            return NoContent();
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<UserDTO>> GetUserById(int id)
         {
