409f86cd06e6609799f5eef1e00b35e03031d272
Mykola Rostotskyi <1nick.rost@gmail.com>
2022-03-31 21:33:52 +0300

    fixed comments date and video underline

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://localhost:5001/api',
+  apiUrl: 'https://localhost:44316/api',
   blobApiUrl: 'https://localhost:5001/api',
   production: false,
   googleKey: '',
